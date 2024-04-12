752972a05b1c47bfa9becd768ee5990948f6718a
mangushev-ildar <mangushev.ildar@bk.ru>
2024-04-12 18:12:55 +0300

    start L1

--- makefile.sh
+++ makefile.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 for i in {00..26}; do
-mkdir "$i"
+    directory="$i"
+    mkdir -p "$directory"
+    if [ -d "$directory" ]; then
+        echo "Entering directory $directory"
+        cd "$directory" || exit 1
+        touch "main.go"
+        echo "Created file in $directory"
+        cd ..
+    else
+        echo "Directory $directory does not exist"
+    fi
 done
