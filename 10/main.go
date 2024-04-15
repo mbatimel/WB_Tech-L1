080dd570f053fea8d4827a3c7bdfdf219e532891
mangushev-ildar <mangushev.ildar@bk.ru>
2024-04-15 12:56:55 +0300

    10

--- 10/main.go
+++ 10/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func getKey(value float64) int64{
+	return int64(value - math.Mod(value, 10))
+}
+func main() {
+	data := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	mp := make(map[int64] []float64)
+	for _, temp := range data {
+		
+		key :=getKey(temp)
+		mp[key] = append(mp[key], temp)
+	}
+
+	
+	for key, value := range mp {
+		fmt.Println(key, value)
+	}
+
+}
