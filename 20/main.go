d7acb120b179267e28e603f5b5344f1225c273d2
mangushev-ildar <mangushev.ildar@bk.ru>
2024-04-16 17:45:44 +0300

    20

---
+++ 20/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	rs := "snow dog sun"
+	fmt.Println(SortStart(rs))
+
+}
+
+func quickSort(arr []string, low, high int) []string {
+	
+	i := low
+	for j := high; j > high/2; j-- {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		
+	}
+	
+	return arr
+}
+
+func SortStart(str string) []string {
+	split_str:= strings.Split(str," ")
+	return quickSort(split_str, 0, len(split_str)-1)
+}
